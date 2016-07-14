b69defbd2b657974c3cbfe99cc9263381d2e9633
Krazeelegz <krish.jolliffe@gmail.com>
2016-07-14 16:30:15 -0700

    moved out of index

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  root 'application#home'
+
+  get "/defaultsite" => redirect("/home")
+  
+  get 'home', to: 'application#home'
+
+  
 end
