f5a3dc0f740de8fa2cb0c78523f2c8d2b40497bf
Krazeelegz <krish.jolliffe@gmail.com>
2016-07-14 17:08:24 -0700

    fuckfuck

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -47,7 +47,7 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
-  config.public_file_server.enabled = true
+  config.public_file_server.enabled = false
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
